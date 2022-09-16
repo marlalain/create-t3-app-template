3d5bd12d384e9d6e91266b0fc617c4b2589bca2f
Alex <@.gg>
2022-09-16 05:40:09 -0300

    feat(trpc): adds example query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { trpc } from "../utils/trpc";
 import styles from "./index.module.css";
 
 const Home: NextPage = () => {
+  const { data } = trpc.example.hello.useQuery({ text: "from tRPC" });
+
   return (
     <>
       <Head>
@@ -48,6 +51,10 @@ const Home: NextPage = () => {
               description="Build data-driven JavaScript & TypeScript apps in less time"
               documentation="https://www.prisma.io/docs/"
             />
+          </div>
+
+          <div className={styles.helloFrom}>
+            {data ? <p>{data.greeting}</p> : <p>Loading...</p>}
           </div>
         </div>
       </div>
